348ef88422b3bb4dc0ac4dfd790db1bee23a083a
Yuki Okada <okada126@gmail.com>
2023-12-20 09:56:38 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    time.sleep(180)  # sleep 3 minutes
+    return "Hello, OpenShift!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
